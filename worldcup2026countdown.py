0da9bb9a5adaa6e828c5ef4b1a1786e9e15e82f8
Leo Zingaya <19838800+zingaya@users.noreply.github.com>
2022-12-21 16:51:00 -0300

    Update worldcup2026countdown.py

--- worldcup2026countdown.py
+++ worldcup2026countdown.py
@@ -24,7 +24,7 @@ def countdown():
     minutes, seconds = divmod(remainder, 60)
 
     # Create a json with the remaining time
-    data = {'year': years, 'months': months, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds, 'unix_time': remaining_time}
+    data = {'year': years, 'months': months, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds, 'unix_time': remaining_time, 'app_ver': '1.0'}
 
     return json.dumps(data)
 
